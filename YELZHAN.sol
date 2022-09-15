1c8b0bce49e97d6ee0f0c15ad0c98fd3290e87c4
Yelzhan Umbetov <104549101+Ye1eke@users.noreply.github.com>
2022-09-15 18:05:34 +0600

    Update YELZHAN.sol

--- YELZHAN.sol
+++ YELZHAN.sol
@@ -6,11 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract YLZToken is ERC20, ERC20Burnable, Ownable {
+contract YELZHAN is ERC20, ERC20Burnable, Ownable {
 
   mapping(address => bool) controllers;
   
-  constructor() ERC20("Yelzhan", "YLZ") { }
+  constructor() ERC20("YELZHAN", "YLZ") { }
 
   function mint(address to, uint256 amount) external {
     require(controllers[msg.sender], "Only controllers can mint");
